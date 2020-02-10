f68f57600ac31c63e50fb606adf4bbb44aa92688
Jonte <jsmi513@lsu.edu>
2020-02-10 12:27:24 -0600

    configuring for heroku

--- tweeter_app/settings.py
+++ tweeter_app/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'eg=eacbm4=_ym*l_uxhje1%zi632_tj@8#^9y4@!^wk9f)gpf-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
